8b595467cb04400d892097b5a56ff852b7da28a8
Chomentowski Sebastian <chomentowski.s@gmail.com>
2022-04-05 17:32:39 +0200

    merging second sprint files

---
+++ dao/GameDatabaseManager.java
@@ -0,0 +1,39 @@
+package com.codecool.dungeoncrawl.dao;
+
+import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.model.PlayerModel;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class GameDatabaseManager {
+    private PlayerDao playerDao;
+
+    public void setup() throws SQLException {
+        DataSource dataSource = connect();
+        playerDao = new PlayerDaoJdbc(dataSource);
+    }
+
+    public void savePlayer(Player player) {
+        PlayerModel model = new PlayerModel(player);
+        playerDao.add(model);
+    }
+
+    private DataSource connect() throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String dbName = "test";
+        String user = "test";
+        String password = "test";
+
+        dataSource.setDatabaseName(dbName);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+        System.out.println("Trying to connect");
+        dataSource.getConnection().close();
+        System.out.println("Connection ok.");
+
+        return dataSource;
+    }
+}
---
+++ model/BaseModel.java
@@ -0,0 +1,34 @@
+package com.codecool.dungeoncrawl.model;
+
+import java.lang.reflect.Field;
+
+public class BaseModel {
+    // null means not saved
+    protected Integer id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        for (Field field : this.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            Object value = null;
+            try {
+                value = field.get(this);
+                if (value != null) {
+                    sb.append(field.getName() + ":" + value + ",");
+                }
+            } catch (IllegalAccessException e) {
+
+            }
+        }
+        return sb.toString();
+    }
+}
